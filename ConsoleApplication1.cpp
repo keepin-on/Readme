af8bcaf7744ae5b361b075f6894a635e1e7831c4
keepin-on <beatsg000ing@gmail.com>
2025-01-13 09:36:03 +0200

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+
+void taskProd() {
+    int num;
+    long long prod = 1;
+    cout << "Task 1: PROD\nEnter integers (0 to end):" << endl;
+    while (true) {
+        cin >> num;
+        if (num == 0)
+            break;
+        prod *= num;
+    }
+    cout << "PROD = " << prod << endl;
+}
+
+int main() {
+    taskProd();
+    return 0;
+}
